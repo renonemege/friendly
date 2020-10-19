bd5d55e3b323f4de84c8206c671294ad120fb427
Esin Adsoy <esin.adsoy@gmail.com>
2020-10-19 17:27:42 +0300

    added views for friend_groups

--- app/controllers/friend_groups_controller.rb
+++ app/controllers/friend_groups_controller.rb
@@ -1,14 +1,22 @@
 class FriendGroupsController < ApplicationController
   def index
+    @friend_groups = FriendGroup.all
   end
 
   def new
+    @friend_group = FriendGroup.new
+
   end
 
   def create
+
   end
 
   def show
+    set_friend_group
+    @review = Review.new
+
+
   end
 
   def edit
@@ -19,4 +27,16 @@ class FriendGroupsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_friend_group
+      @friend_group = FriendGroup.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def friend_group_params
+      params.require(:friend_group).permit(:price, :description, :title, :address)
+    end
 end
