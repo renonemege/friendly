23540c3d84047feaa66ef961b956ad0bcbe5e880
HAKAN BIYIK <hakanbiyik91@gmail.com>
2020-10-20 17:56:30 +0300

    added date section

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,31 @@
 class BookingsController < ApplicationController
+
+  def index
+    @booking = current_user.booking
+  end
+
   def create
+<<<<<<< Updated upstream
+=======
+    @booking = Booking.new(booking_params)
+    @friend_group = FriendGroup.find(params[:friend_group_id])
+    @booking.friend_group = @friend_group
+    @user = User.last.id
+    @booking.user_id = @user
+
+
+    if @booking.save
+      redirect_to friend_group_path(@friend_group)
+    else
+      render 'friend_groups/show'
+    end
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status, :total_price)
+>>>>>>> Stashed changes
   end
 end
