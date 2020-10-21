24bd21520814ba9bd46c45c2e4b96255f211989e
Esin Adsoy <esin.adsoy@gmail.com>
2020-10-21 22:36:42 +0300

    added photo upload function

--- app/models/friend_group.rb
+++ app/models/friend_group.rb
@@ -2,6 +2,7 @@ class FriendGroup < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings, dependent: :destroy
+  has_one_attached :photo
   validates :price, :description, :title, :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
