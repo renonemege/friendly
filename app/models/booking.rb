b0ceb69c2ef0fa615292eabd98d67eb88b851f8e
Esin Adsoy <esin.adsoy@gmail.com>
2020-10-22 15:24:22 +0300

    todays work commited unfinished to try heroku out first

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,15 +1,14 @@
 class Booking < ApplicationRecord
-  before_save :update_total
+  after_validation :update_total
 
   belongs_to :friend_group
   belongs_to :user
   has_many :reviews, dependent: :destroy
-  validates :status, :start_date, :end_date, :total_price, presence: true
+  validates :status, :start_date, :end_date, presence: true
 
   private
 
   def update_total
-    friend_group = FriendGroup.where(booking_id: id)
-    self.total_price = (end_date - start_date).to_i * friend_group.price
+    self.total_price = ((end_date - start_date).to_i * friend_group.price.to_i).to_s
   end
 end
